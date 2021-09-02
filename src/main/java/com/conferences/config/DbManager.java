0035b7bb48f0e42dee9a3dddfa71258f21d43cf9
AndriyBosik <andriybosik2002@ukr.net>
2021-09-02 17:55:36 +0300

    Database data was extracted to application.properties file

--- src/main/java/com/conferences/config/DbManager.java
+++ src/main/java/com/conferences/config/DbManager.java
@@ -1,12 +1,24 @@
 package com.conferences.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public class DbManager {
 
+    private static final String DB_DRIVER = "db.driver";
+    private static final String DB_CONNECTION_URL = "db.connection.url";
+    private static final String DB_USERNAME = "db.username";
+    private static final String DB_PASSWORD = "db.password";
+
     private static DbManager instance;
+
+    private Map<String, String> dbProperties;
 
     private DbManager() {}
 
@@ -18,10 +30,30 @@ public class DbManager {
     }
 
     public Connection getConnection() throws SQLException {
-        if (!org.postgresql.Driver.isRegistered()) {
-            org.postgresql.Driver.register();
+        try {
+            Class.forName(getPropertiesValue(DB_DRIVER));
+        } catch (ClassNotFoundException exception) {
+            exception.printStackTrace();
         }
-        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/conferences?useUnicode=true&characterEncoding=UTF-8&client_encoding=UTF8", "postgres", "postgres");
+        return DriverManager.getConnection(getPropertiesValue(DB_CONNECTION_URL), getPropertiesValue(DB_USERNAME), getPropertiesValue(DB_PASSWORD));
+    }
+
+    public String getPropertiesValue(String key) {
+        if (dbProperties != null) {
+            return dbProperties.get(key);
+        }
+        try (InputStream input = DbManager.class.getClassLoader().getResourceAsStream("application.properties")) {
+            Properties properties = new Properties();
+            properties.load(input);
+            dbProperties = new HashMap<>();
+            for (Map.Entry<Object, Object> entry: properties.entrySet()) {
+                dbProperties.put((String) entry.getKey(), (String) entry.getValue());
+            }
+            return dbProperties.get(key);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return null;
     }
 
 }
